b35774834cf6b6004dc54351dc6e01a138bb43b1
mramalhog <marirg28@gmail.com>
2023-08-25 20:52:53 -0300

    unidade01

---
+++ unidade 01/ex04.ts
@@ -0,0 +1,24 @@
+// P.1.4. Escreva um programa, em TypeScript, que solicite que o usuário digite duas letras e diga qual delas vem antes e qual vem depois no alfabeto.
+
+class Antes {
+    a: string;
+    b: string;
+    
+    constructor(a: string, b: string) {
+      this.a = a;
+      this.b = b;
+    }
+
+    print_mrg(){
+        if (this.a<this.b){
+            console.log(`Letra ${this.a} é anterior a ${this.b}`);
+        }
+        else{
+            console.log(`Letra ${this.b} é anterior a ${this.a}`);
+        }
+    }
+    
+}
+
+const teste2 = new Antes("C","D");
+teste2.print_mrg();
